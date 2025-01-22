9495626786f64a402828afe3d8096db11e32e761
wilfriedtayou <wilfriedtayou6@gmail.com>
2025-01-22 09:33:00 +0000

    note

--- note.py
+++ note.py
@@ -1,28 +1,17 @@
-class Apprenant:
-    def __init__(self, nom, prenom, id):
-        self.nom = nom
-        self.prenom = prenom
-        self.id = id
+class Note:
+    def __init__(self, valeur, apprenantId, matiereCode):
+        self.valeur = valeur
+        self.apprenantId = apprenantId
+        self.matiereCode = matiereCode
 
     @classmethod
-    def ajouterApprenant(cls, apprenants, nom, prenom, id):
-        for apprenant in apprenants:
-            if apprenant.id == id:
-                print(f"Erreur : Un apprenant avec l'ID {id} existe déjà.")
-                return False
+    def ajouterNote(cls, notes, valeur, apprenantId, matiereCode):
+        notes.append(cls(valeur, apprenantId, matiereCode))
+        print(f"Note de {valeur} ajoutée pour l'apprenant ID {apprenantId} en matière {matiereCode}.")
 
-        # Crée un nouvel apprenant et l'ajoute à la liste
-        nouvel_apprenant = cls(nom, prenom, id)
-        apprenants.append(nouvel_apprenant)
-        print(f"Apprenant '{prenom} {nom}' ajouté avec succès.")
-        return True
-
-    @classmethod
-    def afficherApprenants(cls, apprenants):
-        if not apprenants:
-            print("Aucun apprenant enregistré.")
-            return
-
-        print("Liste des apprenants :")
-        for apprenant in apprenants:
-            print(f"ID: {apprenant.id}, Nom: {apprenant.nom}, Prénom: {apprenant.prenom}")
+    @staticmethod
+    def afficherNotesApprenant(notes, apprenantId):
+        print(f"\nNotes pour l'apprenant ID {apprenantId} :")
+        for note in notes:
+            if note.apprenantId == apprenantId:
+                print(f"Matière Code : {note.matiereCode}, Note : {note.valeur}")
